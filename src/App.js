2f14b9f49763c38534e8ee037fbb8373953af43c
vinaysingh <vinay.singh@dhwaniris.com>
2019-06-11 18:53:52 +0530

    adding context to component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BASE_URI } from './services'
+
 import LandingPage from './components/landingPage';
 import SearchResults from './components/searchResults';
 import PageNotFound from './components/pageNotFound';
@@ -8,9 +10,36 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CarRentalContext from './context'
 
 const App = () => {
+
+  const [data, setData] = useState([])
+  const [locationList, setlocationList] = useState([])
+  const [selectedLocation, setSelectedLocation] = useState('')
+  const [selectedDay, setSelectedDay] = useState('')
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      let response = await fetch(`${BASE_URI}`)
+      let json = await response.json()
+      if (json.length) {
+        setData(json)
+        let uniqueLocationArray = [...new Set(json.map((ln) => ln.location))];
+        setlocationList(uniqueLocationArray)
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
   return (
     <div className="App">
-      <CarRentalContext.Provider>
+      <CarRentalContext.Provider value={{
+        locations: locationList, carsData: data,
+        day: selectedDay, location: selectedLocation
+      }}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={LandingPage} />
