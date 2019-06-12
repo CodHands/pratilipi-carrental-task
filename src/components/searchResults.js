dd9d468847b0724d5aa728c4c8601b97a887b67a
vinaysingh <vinay.singh@dhwaniris.com>
2019-06-12 18:37:20 +0530

    added pagination

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -1,17 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
 import CarFilters from './filters';
+import Pagination from "react-js-pagination";
 import carRentalContext from '../context';
 
 const SearchResults = () => {
     const { selectedDay, selectedLocation, carsData } = useContext(carRentalContext)
     const [filteredCards, setFilteredCards] = useState([])
     const [selectedCars, setSelectedCars] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
 
     useEffect(() => {
         const updated_cars = carsData.filter((car) => car.availability.includes(selectedDay) && car.location === selectedLocation);
         setSelectedCars(updated_cars);
         setFilteredCards(updated_cars)
-    }, [])
+    }, [selectedCars])
 
     const handleFuelTypeChange = (e) => {
         const cars = filteredCards.filter((car) => car.fuel_Type === e.target.value);
@@ -25,10 +27,40 @@ const SearchResults = () => {
 
     const handleTransmission = (e) => {
         const cars = filteredCards.filter((car) => car.transmission === e.target.value);
-        console.log(cars);
-
         setSelectedCars(cars)
     }
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page)
+    }
+
+    const paginatedCars = () => {
+        const lastIndex = currentPage * 6;
+        const firstIndex = lastIndex - 6;
+        const updatedCars = selectedCars.slice(firstIndex, lastIndex);
+
+        return updatedCars.map((car, i) => {
+            return <div className="cars-box py-3 px-3 mb-3" key={i}>
+                <div className="car-photo">
+                    <img src={car.photo} alt="car_photo" />
+                </div>
+                <div className="car-specifications mt-3">
+                    <p className="car-location mb-1"><i>{car.location}</i></p>
+                    <h4>{car.name}</h4>
+                    <ul>
+                        <li>{car.seats}</li>
+                        <li>{car.fuel_Type}</li>
+                        <li>{car.transmission}</li>
+                    </ul>
+                </div>
+                <div className="car-price">
+                    Rs. {car.price}
+                </div>
+            </div>
+        });
+    }
+
+    console.log(selectedCars);
 
     return (
         <div className="cars-container py-5">
@@ -37,27 +69,13 @@ const SearchResults = () => {
                 changeFuelType={handleFuelTypeChange}
                 changeTransmission={handleTransmission}
             />
-            {selectedCars.length ? (
-                selectedCars.map((car, i) => {
-                    return <div className="cars-box py-3 px-3 mb-3" key={i}>
-                        <div className="car-photo">
-                            <img src={car.photo} alt="car_photo" />
-                        </div>
-                        <div className="car-specifications mt-3">
-                            <p className="car-location mb-1"><i>{car.location}</i></p>
-                            <h4>{car.name}</h4>
-                            <ul>
-                                <li>{car.seats}</li>
-                                <li>{car.fuel_Type}</li>
-                                <li>{car.transmission}</li>
-                            </ul>
-                        </div>
-                        <div className="car-price">
-                            Rs. {car.price}
-                        </div>
-                    </div>
-                })
-            ) : null}
+            {selectedCars.length ? paginatedCars() : null}
+            <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={6}
+                totalItemsCount={selectedCars.length}
+                onChange={handlePageChange}
+            />
         </div>
     )
 }
