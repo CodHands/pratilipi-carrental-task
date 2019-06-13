6e2ef2451d32f174a454261f6aefbb7f121d6d5c
vinaysingh <vinay.singh@dhwaniris.com>
2019-06-13 17:44:45 +0530

    changes to responsive layour

--- src/components/filters.js
+++ src/components/filters.js
@@ -6,25 +6,25 @@ const CarFilters = ({ changeTransmission, changeCarType, changeFuelType, handleS
 
     return (
         <div className="car-filters row">
-            <div className="form-group col-md-3">
+            <div className="form-group col-md-3 col-sm-3 col-sx-6">
                 <select className="form-control" defaultValue="" onChange={(e) => changeCarType(e)}>
                     <option value="" disabled>----Select Car Type----</option>
                     {[...new Set(carsData.map((ln) => ln.car_Type))].map((el, i) => <option key={i}>{el}</option>)}
                 </select>
             </div>
-            <div className="form-group col-md-3">
+            <div className="form-group col-md-3 col-sm-3 col-sx-6">
                 <select className="form-control" defaultValue="" onChange={(e) => changeFuelType(e)}>
                     <option value="" disabled>----Select Fuel Type----</option>
                     {[...new Set(carsData.map((ln) => ln.fuel_Type))].map((el, i) => <option key={i}>{el}</option>)}
                 </select>
             </div>
-            <div className="form-group col-md-3">
+            <div className="form-group col-md-3 col-sm-3 col-sx-6">
                 <select className="form-control" defaultValue="" onChange={(e) => changeTransmission(e)}>
                     <option value="" disabled>----Select Transmission----</option>
                     {[...new Set(carsData.map((ln) => ln.transmission))].map((el, i) => <option key={i}>{el}</option>)}
                 </select>
             </div>
-            <div className="car-sort col-md-3" title="sort by price">
+            <div className="car-sort col-md-3 col-sm-3 col-sx-6" title="sort by price">
                 <input type="text" className="search-box" onKeyUp={(e) => handleSearch(e)} placeholder="Search your car" />
                 <img src="/images/sort.svg" width="30" alt="sort-icon" onClick={handleSorting} />
             </div>
